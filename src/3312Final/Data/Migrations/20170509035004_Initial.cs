0cce7ee15a868dd2b215f30065ccfeef1524ba81
Trent Nettles <dtnettles1@buffs.wtamu.edu>
2017-05-09 04:10:15 +0000

    First commit

---
+++ src/3312Final/Data/Migrations/20170509035004_Initial.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _3312Final.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FinalApproachSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FlapInitRetractSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FlapRetractSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GoAroundSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaintainSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinTakeoffSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RotationSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SafeSpeed",
+                table: "Airplane",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinalApproachSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "FlapInitRetractSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "FlapRetractSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "GoAroundSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "MaintainSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "MinTakeoffSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "RotationSpeed",
+                table: "Airplane");
+
+            migrationBuilder.DropColumn(
+                name: "SafeSpeed",
+                table: "Airplane");
+        }
+    }
+}
---
+++ src/AirplaneLibrary/AirplaneOutputs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirplaneLibrary
+{
+    public class AirplaneOutputs
+    {
+       public static int V1(int Vr)
+       {
+            return Vr;
+       }
+     }
+}
+
+
